d857d90935a98807552f7696fcfed504e4a6bb5a
Prosper Ogbonnaya <ogbonnayaprosper1@gmail.com>
2022-12-05 19:31:54 +0000

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDx5cvBxRgk-XTSRFtNeWyZuOCPC600q7k",
+  authDomain: "naijastoreweb.firebaseapp.com",
+  projectId: "naijastoreweb",
+  storageBucket: "naijastoreweb.appspot.com",
+  messagingSenderId: "35077693326",
+  appId: "1:35077693326:web:2af062d4be621bc72d4edf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
