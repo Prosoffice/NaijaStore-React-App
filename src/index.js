a8c2360fb72786ed414a10c18438e3e8e64331a5
Prosoffice <ogbonnayaprosper1@gmail.com>
2022-12-14 00:14:04 +0000

    complete checkout page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer, {initialState} from './reducer';
+import { StateProvider } from './StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+    </StateProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/reducer.js
@@ -0,0 +1,44 @@
+export const initialState = {
+    basket: []
+};
+
+// Selector
+export const getBasketTotal = (basket) => 
+    basket?.reduce((amount, item) => item.price + amount, 0);
+
+
+
+const reducer = (state, action) => {
+    console.log(action);
+    switch(action.type) {
+        case 'ADD_TO_BASKET':
+            return {
+                ...state,
+                basket: [...state.basket, action.item]
+            };
+
+        case 'REMOVE_FROM_BASKET':
+            const indexToBeRemoved = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+            let newBasket = [...state.basket]
+
+            if (indexToBeRemoved >= 0) {
+                newBasket.splice(indexToBeRemoved, 1);
+            } else {
+                console.warn("Index do not exist")
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+             }
+
+        default:
+            return state
+    }
+};
+
+
+export default reducer
